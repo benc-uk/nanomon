066332c6a42b1df32a903c44109f582bd460f13d
Ben Coleman <benc.uk@gmail.com>
2023-05-21 17:58:26 +0100

    Enhance: reduce number of API calls 🧠

--- frontend/views/home.mjs
+++ frontend/views/home.mjs
@@ -6,6 +6,7 @@
 import { getStatusFields, monitorIcon } from '../lib/utils.mjs'
 
 const charts = {}
+const CHART_SIZE = 20
 
 export const homeComponent = (api) => ({
   monitors: [],
@@ -66,8 +67,15 @@ export const homeComponent = (api) => ({
     this.loading = false
     this.updateText = new Date().toLocaleTimeString()
 
+    // Collect all results for all monitors, needed for charts
+    let allResults = {}
+
     for (const m of monitorsNew) {
-      const results = await api.getResultsForMonitor(m.id, 1)
+      const results = await api.getResultsForMonitor(m.id, CHART_SIZE)
+
+      // Push results into allResults object
+      allResults[m.id] = results
+
       const last = new Date(results[0]?.date)
       m.message = results[0]?.message
       m.lastRan = results[0]?.date ? last.toLocaleString() : 'Never'
@@ -77,12 +85,12 @@ export const homeComponent = (api) => ({
 
     this.monitors = monitorsNew
 
-    await this.updateCharts()
+    await this.updateCharts(allResults)
   },
 
-  async updateCharts() {
+  async updateCharts(allResults) {
     for (const m of this.monitors) {
-      const results = await api.getResultsForMonitor(m.id, 20)
+      const results = allResults[m.id]
 
       // Build data arrays for chart
       const resultValues = []
@@ -102,40 +110,42 @@ export const homeComponent = (api) => ({
       }
 
       // Create new chart otherwise
-      const ctx = document.getElementById(`chart_${m.id}`)
-      charts[m.id] = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: resultLabels,
-          datasets: [
-            {
-              label: 'Response time',
-              data: resultValues,
-            },
-          ],
-        },
-        options: {
-          plugins: {
-            legend: {
-              display: false,
-            },
-            tooltip: {
-              callbacks: {
-                label: function (context) {
-                  return `${context.parsed.y} ms`
+      this.$nextTick(() => {
+        const ctx = document.getElementById(`chart_${m.id}`)
+        charts[m.id] = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: resultLabels,
+            datasets: [
+              {
+                label: 'Response time',
+                data: resultValues,
+              },
+            ],
+          },
+          options: {
+            plugins: {
+              legend: {
+                display: false,
+              },
+              tooltip: {
+                callbacks: {
+                  label: function (context) {
+                    return `${context.parsed.y} ms`
+                  },
                 },
               },
             },
-          },
-          scales: {
-            y: {
-              beginAtZero: true,
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+              x: {
+                display: false,
+              },
             },
-            x: {
-              display: false,
-            },
           },
-        },
+        })
       })
     }
   },
